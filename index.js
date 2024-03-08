7e2780c4580ed32963d3db8d01289ebc206f8a06
rahuljat1349 <rahuldudi1349@gmail.com>
2024-03-08 15:19:56 +0530

    add cors options

--- index.js
+++ index.js
@@ -4,14 +4,15 @@ const cors = require('cors');
 dbConnect();
 
 const app = express();
-const port = 8000;
-
-app.use(cors());
+const corsOptions = {
+  origin: "*"
+};
+app.use(cors(corsOptions));
 app.use(express.json());
 // Avilable Routes
 app.use("/api/auth", require("./routes/auth"));
 app.use('/api/notes',require('./routes/notes'));
 
 app.listen(port, () => {
-  console.log(`srever is listening on post ${port}`);
+  console.log(`srever is started`);
 });
